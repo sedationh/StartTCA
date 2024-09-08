b6ca1d620bb12cf035cc593a65ad3f5e757226b8
SedationH <sedationh@gmail.com>
2024-09-08 15:33:06 +0800

    Initial Commit

---
+++ StartTCA2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StartTCA2
+//
+//  Created by Holden Hu on 2024/9/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StartTCA2/StartTCA2App.swift
@@ -0,0 +1,17 @@
+//
+//  StartTCA2App.swift
+//  StartTCA2
+//
+//  Created by Holden Hu on 2024/9/8.
+//
+
+import SwiftUI
+
+@main
+struct StartTCA2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
